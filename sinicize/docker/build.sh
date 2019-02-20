6a5dfac757680aa26ec49c9ebd1d72baa1c905d3
gaojun <gaojun@fudan.edu.cn>
2019-02-20 17:07:39 +0800

    完善Docker配置

--- sinicize/docker/build.sh
+++ sinicize/docker/build.sh
@@ -6,4 +6,11 @@ set -e
 cd $(dirname $(readlink -f $0))
 
 export CANVAS_VERSION='20190105'
+
+# RubyGems镜像
+# 阿里云
+export RUBYGEMS_MIRROR='https://mirrors.aliyun.com/rubygems/'
+# 清华
+# export RUBYGEMS_MIRROR='https://mirrors.tuna.tsinghua.edu.cn/rubygems'
+
 docker-compose -f docker-compose.build.yml build
