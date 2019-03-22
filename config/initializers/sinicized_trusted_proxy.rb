6e38ce19bcba1971cb1cd466cda7d681dc39e68d
gaojun <gaojun@fudan.edu.cn>
2019-03-22 23:59:26 +0800

    通过参数TRUSTED_PROXY_PATTERN和TRUSTED_PROXIES，解决部署在局域网中无法正常识别IP，以及测验IP过滤失效的问题

--- config/initializers/sinicized_trusted_proxy.rb
+++ config/initializers/sinicized_trusted_proxy.rb
@@ -5,10 +5,20 @@ module Rack
       alias origin_trusted_proxy? trusted_proxy?
 
       def trusted_proxy?(ip)
-        return origin_trusted_proxy?(ip) unless ENV['TRUSTED_PROXY']
-        @trusted_proxy_pattern ||= Regexp.new(ENV['TRUSTED_PROXY'])
+        return origin_trusted_proxy?(ip) unless ENV['TRUSTED_PROXY_PATTERN']
+        @trusted_proxy_pattern ||= Regexp.new(ENV['TRUSTED_PROXY_PATTERN'])
         ip =~ @trusted_proxy_pattern
       end
+    end
+  end
+end
+
+module CanvasRails
+  class Application
+    if ENV['TRUSTED_PROXIES']
+      require "ipaddr"
+      trusted_proxies = ENV['TRUSTED_PROXIES'].split(',').map{|proxy| IPAddr.new proxy}
+      config.action_dispatch.trusted_proxies = trusted_proxies
     end
   end
 end
