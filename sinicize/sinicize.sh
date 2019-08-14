b21438e625ed366620376d5ae2feac5822a06bf5
gaojun <gaojun@fudan.edu.cn>
2019-08-14 15:58:21 +0800

    改进第三方包镜像的配置

--- sinicize/sinicize.sh
+++ sinicize/sinicize.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-
-GITHUB_PREFIX="https://github.com/"
+GITHUB_PREFIX_HTTPS="https://github.com/"
+GITHUB_PREFIX_GIT="git://github.com/"
+#GITHUB_PREFIX_CODELOAD="https://codeload.github.com"
 GITHUB_MIRROR_PREFIX="https://source.fudan.edu.cn/gitlab/canvas/mirrors/"
 YARNPKG_PREFIX="https://registry.yarnpkg.com/"
 YARNPKG_MIRROR_PREFIX="https://npmreg.proxy.ustclug.org/"
@@ -12,14 +13,14 @@ echo "$(dirname $(readlink -f $0))/.."
 cd $(dirname $(readlink -f $0))/..
 
 echo "重定向 Gemfile的GitHub库"
-echo "  GitHub镜像: ${GITHUB_PREFIX} -> ${GITHUB_MIRROR_PREFIX}"
-sed -i "s,${GITHUB_PREFIX},${GITHUB_MIRROR_PREFIX},g" Gemfile.d/_before.rb
+echo "  GitHub镜像: ${GITHUB_PREFIX_HTTPS} -> ${GITHUB_MIRROR_PREFIX}"
+sed -i "s,${GITHUB_PREFIX_HTTPS},${GITHUB_MIRROR_PREFIX},g" Gemfile.d/_before.rb
 
 echo "处理 package.json:"
-echo "  GitHub镜像: ${GITHUB_PREFIX} -> ${GITHUB_MIRROR_PREFIX}"
-sed -i "s,${GITHUB_PREFIX},${GITHUB_MIRROR_PREFIX},g" package.json
+echo "  GitHub镜像: ${GITHUB_PREFIX_HTTPS} -> ${GITHUB_MIRROR_PREFIX}"
+sed -i "s,${GITHUB_PREFIX_HTTPS},${GITHUB_MIRROR_PREFIX},g" package.json
 
 echo "处理所有 yarn.lock:"
-echo "  GitHub镜像: ${GITHUB_PREFIX} -> ${GITHUB_MIRROR_PREFIX}"
+echo "  GitHub镜像: ${GITHUB_PREFIX_HTTPS} -> ${GITHUB_MIRROR_PREFIX}"
 echo "  yarnpkg镜像: ${YARNPKG_PREFIX} -> ${YARNPKG_MIRROR_PREFIX}"
-find . -name yarn.lock -exec sed -i "s,${GITHUB_PREFIX},${GITHUB_MIRROR_PREFIX},g;s,${YARNPKG_PREFIX},${YARNPKG_MIRROR_PREFIX},g" {} +
+find . -name yarn.lock -exec sed -i "s,${GITHUB_PREFIX_HTTPS},${GITHUB_MIRROR_PREFIX},g;s,${GITHUB_PREFIX_GIT},${GITHUB_MIRROR_PREFIX},g;s,${YARNPKG_PREFIX},${YARNPKG_MIRROR_PREFIX},g" {} +
